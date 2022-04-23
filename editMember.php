1c310c8929a5bd71f0e347aaa633d39a5f90a902
Blair Burns <blairb0986@gmail.com>
2022-04-23 14:00:29 +1000

    minor bug fix

--- editMember.php
+++ editMember.php
@@ -23,21 +23,21 @@ session_start();
 	<form method="post" action="controllers/updateMember.php">
 		<fieldset>
 			<h3>Personal Details</h3>
-			<label for="First_name">Member Id: </label><input type="text" name="memberID" value=" <?php echo $_SESSION['memberID']; ?>" readonly><br><br>
+			<label for="First_name">Member Id: </label><input type="text" name="memberID" value="<?php echo $_SESSION['memberID']; ?>" readonly><br><br>
 
-			<label for="First_name">First name: </label><input type="text" name="firstname" value=" <?php echo $_SESSION['memFirstName']; ?>" ><br><br>
+			<label for="First_name">First name: </label><input type="text" name="firstname" value="<?php echo $_SESSION['memFirstName']; ?>" ><br><br>
 
-			<label for="Last_name">Last name: </label><input type="text"  name="lastname" value=" <?php echo $_SESSION['memLastName']; ?>"><br><br>
+			<label for="Last_name">Last name: </label><input type="text"  name="lastname" value="<?php echo $_SESSION['memLastName']; ?>"><br><br>
 
-			<label for="Email">Email Address: </label><input type="text" name="email" value=" <?php echo $_SESSION['memEmail']; ?>" size="40"><br><br>
+			<label for="Email">Email Address: </label><input type="text" name="email" value="<?php echo $_SESSION['memEmail']; ?>" size="40"><br><br>
 
-			<label for="Phone">Phone number: </label><input type="text" name="mobile" value=" <?php echo $_SESSION['memMobile']; ?>" ><br><br>
+			<label for="Phone">Phone number: </label><input type="text" name="mobile" value="<?php echo $_SESSION['memMobile']; ?>" ><br><br>
 
-			<label for="address">Address: </label><input type="text" name="streetAddress" value=" <?php echo $_SESSION['memAddress']; ?>"><br><br>
+			<label for="address">Address: </label><input type="text" name="streetAddress" value="<?php echo $_SESSION['memAddress']; ?>"><br><br>
 
-			<label for="suburb">Suburb: </label><input type="text" name="suburb" value=" <?php echo $_SESSION['memSuburb']; ?>"><br><br>
+			<label for="suburb">Suburb: </label><input type="text" name="suburb" value="<?php echo $_SESSION['memSuburb']; ?>"><br><br>
 
-			<label for="post_code">Postcode: </label><input type="text" name="postcode" value=" <?php echo $_SESSION['memPostcode']; ?>"><br><br>
+			<label for="post_code">Postcode: </label><input type="text" name="postcode" value="<?php echo $_SESSION['memPostcode']; ?>"><br><br>
 			<hr>
 			<button type="submit" class="registerbtn">Save</button>
 		</fieldset>
--- search.php
+++ search.php
@@ -24,21 +24,21 @@ session_start();
 
 	<fieldset>
 		<h3>Personal Details</h3>
-		<label for="First_name">Member Id: </label><input type="text" value=" <?php echo $_SESSION['memberID']; ?>" disabled><br><br>
+		<label for="First_name">Member Id: </label><input type="text" value="<?php echo $_SESSION['memberID']; ?>" disabled><br><br>
 
-		<label for="First_name">First name: </label><input type="text" value=" <?php echo $_SESSION['memFirstName']; ?>" disabled><br><br>
+		<label for="First_name">First name: </label><input type="text" value="<?php echo $_SESSION['memFirstName']; ?>" disabled><br><br>
 
-		<label for="Last_name">Last name: </label><input type="text" value=" <?php echo $_SESSION['memLastName']; ?>" disabled><br><br>
+		<label for="Last_name">Last name: </label><input type="text" value="<?php echo $_SESSION['memLastName']; ?>" disabled><br><br>
 
-		<label for="Email">Email Address: </label><input type="text" value=" <?php echo $_SESSION['memEmail']; ?>" disabled size="40"><br><br>
+		<label for="Email">Email Address: </label><input type="text" value="<?php echo $_SESSION['memEmail']; ?>" disabled size="40"><br><br>
 
-		<label for="Phone">Phone number: </label><input type="text" value=" <?php echo $_SESSION['memMobile']; ?>" disabled><br><br>
+		<label for="Phone">Phone number: </label><input type="text" value="<?php echo $_SESSION['memMobile']; ?>" disabled><br><br>
 
-		<label for="address">Address: </label><input type="text" value=" <?php echo $_SESSION['memAddress']; ?>" disabled><br><br>
+		<label for="address">Address: </label><input type="text" value="<?php echo $_SESSION['memAddress']; ?>" disabled><br><br>
 
-		<label for="suburb">Suburb: </label><input type="text" value=" <?php echo $_SESSION['memSuburb']; ?>" disabled><br><br>
+		<label for="suburb">Suburb: </label><input type="text" value="<?php echo $_SESSION['memSuburb']; ?>" disabled><br><br>
 
-		<label for="post_code">Post Code: </label><input type="text" value=" <?php echo $_SESSION['memPostcode']; ?>" disabled><br><br>
+		<label for="post_code">Post Code: </label><input type="text" value="<?php echo $_SESSION['memPostcode']; ?>" disabled><br><br>
 
 		<hr>
 		<form method="post" action="sales.php">
