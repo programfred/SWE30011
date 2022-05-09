ab8e092376138c52694d473465d323c0733a5c54
Fred Zhou <programfred@gmail.com>
2022-05-09 18:12:58 +1000

    list member use table

--- controllers/listmember.php
+++ controllers/listmember.php
@@ -4,13 +4,33 @@ session_start();
 $sqlStr="select * from tblMember";
 $result = $conn->query($sqlStr);
 ?>
-<ol start="50">
-    <?php
 
-while($row = $result->fetch_assoc()) {
-    echo "<li>". "memberId: " . $row["memberID"]. $row["firstName"]. " " . $row["lastName"]. $row["email"]. $row["mobile"].
-    "<a href=./delmember.php?id=".$row["memberID"].">delete</a>".
-     "</li>";
-  }
-    ?>
-</ol>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<table>
+    <tr>
+        <td>memberId</td>
+        <td>firstName</td>
+        <td>lastName</td>
+        <td>email</td>
+        <td>mobile</td>
+    </tr>
+    <?php while($row = $result->fetch_assoc()) { ?>
+    <tr>
+        <td><?php echo  $row["memberID"]; ?></td>
+        <td><?php echo  $row["firstName"]; ?></td>
+        <td><?php echo  $row["lastName"]; ?></td>
+        <td><?php echo  $row["email"]; ?></td>
+        <td><?php echo  $row["mobile"]; ?></td>
+        <td> <a href=./delmember.php?id=".$row["memberID"].">delete</a></td>
+    </tr>
+    <?php   } ?>
+</table>
+</body>
+</html>
