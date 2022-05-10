bab4d2e516ac0aeb9bb63ee656b3edc69668e2fc
Blair Burns <blairb0986@gmail.com>
2022-05-10 15:39:30 +1000

    create report tables

--- reports.php
+++ reports.php
@@ -37,10 +37,14 @@ session_start();
 			<div class="shape"></div>
 	</div>
 	<div>
-		<button id="weekSales">Weekly Sales</button>
-		<button id="memberReport">Member Report</button>
-		<button id="topProduct">Top Product Report</button>
-		<button id="StockOrderList">Stock Order List</button>
+		<button id="weekSales" onclick="weekSales()">Weekly Sales</button>
+		<div id="1"></div>
+		<button id="memberReport"  onclick="memberReport()">Member Report</button>
+		<div id="2"></div>
+		<button id="topProducts"  onclick="topProducts()">Top Products Report</button>
+		<div id="3"></div>
+		<button id="StockOrderList" onclick="stockOrderList()">Stock Order List</button>
+		<div id="4"></div>
 	</div>
 
 
@@ -48,4 +52,58 @@ session_start();
 	<p id="footertop">© Copyright 2022</p>
 	<p id="footerbottom">All Rights Reserved. Powered by the best team in SWE30011.</p>
 </body>
+<script>
+	function weekSales(){
+		fetch('controllers/AJAX/getWeeklySales.php').then(response => response.json())
+		.then(data => {
+		createTable(data, "1");
+
+		});
+	}
+
+	function memberReport(){
+		console.log("b");
+	}
+
+	function topProducts(){
+		console.log("c");
+	}
+
+	function stockOrderList(){
+		console.log("d");
+	}
+
+
+
+	function createTable(json, tableId){
+		var table = document.createElement("TABLE");
+		table.border = "1";
+
+		//Get the count of columns.
+		var columnCount = Object.keys(json[0]).length;
+
+		console.log(Object.keys(json[3]))
+
+		//Add the header row.
+		var row = table.insertRow(-1);
+		for (var i = 0; i < columnCount; i++) {
+			var headerCell = document.createElement("TH");
+			headerCell.innerHTML = Object.keys(json[0])[i];
+			row.appendChild(headerCell);
+		}
+
+		//Add the data rows.
+		for (var i = 1; i < json.length; i++) {
+			row = table.insertRow(-1);
+			for (var j = 0; j < columnCount; j++) {
+				var cell = row.insertCell(-1);
+				cell.innerHTML = Object.values(json[i])[j];
+			}
+		}
+
+		var dvTable = document.getElementById(tableId);
+		dvTable.innerHTML = "";
+		dvTable.appendChild(table);
+	}
+</script>
 </html>
